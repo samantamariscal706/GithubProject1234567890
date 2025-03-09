e96823db861a354d1c174917c800306ebef7492f
samantamariscal706 <samantamariscal706@gmail.com>
2025-03-09 12:23:22 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class GithubProject1234567890 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = sc.nextInt();
+    if (num % 2 == 0) {
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
+    }
+  }
+}
