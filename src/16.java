7004bb59d82c713a5e2b1f1ea3a27f2735c5833d
samantamariscal706 <samantamariscal706@gmail.com>
2025-03-22 14:53:26 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,12 @@
+public class MyCode {
+    public static void main(String[] args) {
+        // Example of using a switch statement in Java
+        int number = 10;
+        switch(number) {
+            case 1: System.out.println("Number is 1");
+                break; // Exit the switch block
+            case 2: System.out.println("Number is 2");
+                break; // Another exit from the switch block
+        }
+    }
+}
