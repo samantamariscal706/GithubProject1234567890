75dea598b4855ad997061dbc9d9be1b5afef0340
samantamariscal706 <samantamariscal706@gmail.com>
2025-04-05 05:55:41 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        scanner.close();
+    }
+}
